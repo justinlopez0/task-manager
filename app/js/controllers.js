1f75c4ebb89cbc65c5e72b88a77b51de1e11a9e8
justinlopez0 <justinlopez0@gmail.com>
2013-08-26 21:51:09 -0700

    Angular controller tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,31 @@
 'use strict';
 
 /* Controllers */
+function ctrl1($scope) {
+    $scope.myObject = {
+    value: 0,
+    increment: function(inc) {
+        this.value += typeof inc === 'number' ? inc : 1;
+        }
+    }
+    $scope.myObject.increment(5);
+}
 
+/*
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+    controller('MyCtrl1', [function($scope) {
+        $scope.myObject = {
+            value: 0,
+            increment: function(inc) {
+                this.value += typeof inc === 'number' ? inc : 1;
+            }
+        }
 
-  }])
-  .controller('MyCtrl2', [function() {
+        $scope.myObject.increment();
 
-  }]);
+        $scope.name = 'JJ';
+    }])
+    .controller('MyCtrl2', [function() {
+
+    }]);
+*/
